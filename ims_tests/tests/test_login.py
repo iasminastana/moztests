ed55688d5d17f532ebd1cbcaeef26cbf7d6c44c8
Cosmin Traistaru <cosmin@traista.ru>
2017-10-10 15:09:41 +0300

    use pytest-variables plugin for configuration data

--- ims_tests/tests/test_login.py
+++ ims_tests/tests/test_login.py
@@ -1,11 +1,7 @@
 from ..pages.login import Login
 
-_USERNAME = 'admin'
-_PASSWORD = 'asdf'
-_URL = 'http://172.16.228.215:8080/'
 
-
-def test_login(selenium):
-    login_page = Login(selenium, _URL)
-    dashboard = login_page.login(_USERNAME, _PASSWORD)
+def test_login(selenium, variables):
+    login_page = Login(selenium, variables)
+    dashboard = login_page.login(variables['username'], variables['password'])
     assert dashboard.expected_title == dashboard.get_page_title()
